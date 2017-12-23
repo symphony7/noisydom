c07a0c6757fb57080f7685e05fdd7364d3b2cfc6
symphony7 <33542108+symphony7@users.noreply.github.com>
2017-12-23 18:29:21 +0800

    Initial commit

---
+++ pages/start/start.js
@@ -0,0 +1,100 @@
+// start.js
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  getDataFromOneNet: function(){
+    //从oneNET请求我们的Wi-Fi气象站的数据
+    const requestTask = wx.request({
+      url: 'https://api.heclouds.com/devices/20438676/datapoints?datastream_id=sound&limit=15',
+      header: {
+        'content-type': 'application/json',
+        'api-key': 'uCQBvo8fRcYk0DD16GYLhwZm5Cs='
+      },
+      success: function (res) {
+        //console.log(res.data)
+        //拿到数据后保存到全局数据
+        var app = getApp()
+        app.globalData.sound = res.data.data.datastreams[0]
+        console.log(app.globalData.sound)
+        //跳转到天气页面，根据拿到的数据绘图
+        wx.navigateTo({
+          url: '../wifi_station/tianqi/tianqi',
+        })
+      },
+
+      fail: function (res) {
+        console.log("fail!!!")
+      },
+
+      complete: function (res) {
+        console.log("end")
+      }
+    })
+
+  
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
